173944f6b85b43b698f4ee2157e4f70e70802e31
Brandon Galarza <luminoforos@gmail.com>
2025-06-11 21:23:43 -0600

    Modificación de controlador

--- app/Http/Controllers/FontController.php
+++ app/Http/Controllers/FontController.php
@@ -8,29 +8,31 @@ class FontController extends Controller
 {
     public function getFonts() 
     {
-        $fontsPath = public_path('fonts');
+        $sourcePath = base_path('resources/fonts');
+
+        $publicUrlPath = 'fonts';
+
         $categories = [];
 
-        // Recorrer todas las carpetas dentro de fonts/
-        foreach (File::directories($fontsPath) as $categoryPath) {
+        if (!File::exists($sourcePath)) {
+            return response()->json($categories);
+        }
+
+        foreach (File::directories($sourcePath) as $categoryPath) {
             $categoryName = basename($categoryPath);
 
-            // Obtener subcategorías
             $subCategoryPaths = File::directories($categoryPath);
 
-            // Validar que la categoría tenga al menos una subcategoría
             if (empty($subCategoryPaths)) {
-                continue; // No tiene subcategorías, no la agregamos
+                continue; 
             }
 
             $categories[$categoryName] = [];
 
-            // Recorrer subcategorías dentro de la categoría actual
             foreach ($subCategoryPaths as $subCategoryPath) {
                 $subCategoryName = basename($subCategoryPath);
                 $categories[$categoryName][$subCategoryName] = [];
 
-                // Obtener archivos de fuentes dentro de la subcategoría
                 foreach (File::files($subCategoryPath) as $file) {
                     if (in_array($file->getExtension(), ['ttf', 'otf', 'woff', 'woff2'])) {
                         $fontName = pathinfo($file->getFilename(), PATHINFO_FILENAME);
@@ -38,7 +40,7 @@ class FontController extends Controller
 
                         $categories[$categoryName][$subCategoryName][] = [
                             'name' => $fontName,
-                            'url' => asset("fonts/{$categoryName}/{$subCategoryName}/{$file->getFilename()}"),
+                            'url' => asset("{$publicUrlPath}/{$categoryName}/{$subCategoryName}/{$file->getFilename()}"),
                             'styleName' => $styleName
                         ];
                     }
